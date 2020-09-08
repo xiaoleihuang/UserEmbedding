7399d81ccbda01560b034b9c01b659a031e7c093
xiaolei <xiaoleihuangccnu@gmail.com>
2020-09-08 00:09:52 -0600

    add automatically evaluation script, add two modes, running locally on the desktop and remotely on grid

---
+++ run_evaluator_desktop.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import time
+import sys
+
+dnames = ['amazon_health'] # 'amazon', 'yelp' , 'imdb', 'amazon_health'
+modes = {'skipgrams'} # 'skipgrams', 'word2user', 'doc2user', 'lda2user', 'bert2user'
+cluster_nums = [4, 8, 12]
+sample_modes = ['global', 'decay', 'local']
+run_file = 'run_eval.sh'
+
+with open(run_file, 'w') as wfile:
+    wfile.write('#!/bin/sh\n')
+#    wfile.write('source ~/.bashrc\n')
+
+    for dname in dnames:
+        for mode in modes:
+            for cluster_num in cluster_nums:
+                for s_mode in sample_modes:
+                    wfile.write('python evaluator.py {} {} {} {}\n'.format(
+                        dname, mode, cluster_num, s_mode)
+                    )
+
+process = subprocess.Popen('sh {} > results.txt'.format(run_file), shell=True)
+time.sleep(3)
+os.remove(run_file)
